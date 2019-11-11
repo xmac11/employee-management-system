8a471f728f115085cf2775f49842f0a4eac8d264
Charalampos Makrylakis <c.makrylakis@gmail.com>
2019-11-11 17:02:28 +0200

    Initial commit

---
+++ src/test/java/com/team/ghana/GhanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.ghana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
