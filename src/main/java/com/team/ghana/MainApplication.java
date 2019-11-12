e87d7990c01583f592882a46a2394590eec3f2cf
cloudsquall <cloudsquall88@gmail.com>
2019-11-12 13:00:36 +0200

    removed hanging @Autowired annotation from main

--- src/main/java/com/team/ghana/MainApplication.java
+++ src/main/java/com/team/ghana/MainApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
-	@Autowired
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
