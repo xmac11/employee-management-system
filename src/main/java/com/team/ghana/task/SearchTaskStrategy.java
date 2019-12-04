e312e530c2555a8ea1e53a6225e5e826a6992e77
tsakniask <sirkostas@hotmail.com>
2019-12-05 01:28:25 +0200

    minor update

--- src/main/java/com/team/ghana/task/SearchTaskStrategy.java
+++ src/main/java/com/team/ghana/task/SearchTaskStrategy.java
@@ -6,7 +6,4 @@ public interface SearchTaskStrategy {
 
     List<Task> execute(List<Task> allTasks, String id);
 
-    static SearchTaskStrategy noop() {
-        return (allTasks, id) -> null;
-    }
 }
