86859d9f700af589f7f0f07b18add653eac8ff72
Iosif <12469gr@saeinstitute.edu>
2019-12-09 14:27:37 +0200

    Acceptance Tests For Unit & Employee

---
+++ src/test/java/com/team/ghana/unit/UnitFeature.java
@@ -0,0 +1,75 @@
+package com.team.ghana.unit;
+
+import com.team.ghana.MainApplication;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MainApplication.class},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UnitFeature {
+
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getAllUnits() {
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/units")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                    .andExpect(status().isOk())
+                    .andExpect(content().json(UnitJson.json));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void getUnitById() {
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/units/{unitId}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                        .andExpect(status().isOk())
+                        .andExpect(content().json(UnitJson.json));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    @Test
+    @Ignore
+    public void postunit() {
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.post("/units")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                    .andExpect(status().isOk())
+                    .andExpect(content().json(UnitJson.json));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/team/ghana/unit/UnitJson.java
@@ -0,0 +1,5 @@
+package com.team.ghana.unit;
+
+public class UnitJson {
+    public static String json = "[{\"id\":1,\"name\":\"Software Development\",\"departmentName\":\"IT & Managed Services\"},{\"id\":2,\"name\":\"Quality Assurance\",\"departmentName\":\"IT & Managed Services\"},{\"id\":3,\"name\":\"Research and Development\",\"departmentName\":\"Solutions & Pre-Sales\"},{\"id\":4,\"name\":\"IT Support\",\"departmentName\":\"Technical\"},{\"id\":5,\"name\":\"Auditing\",\"departmentName\":\"Banking & Financial Sector\"},{\"id\":6,\"name\":\"Accounting\",\"departmentName\":\"Banking & Financial Sector\"}]";
+}
